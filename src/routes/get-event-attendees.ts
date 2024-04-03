050fd4d266011b0de4115e7b6870b8117636aac4
Ana Claudia <anacnogueira@gmail.com>
2024-04-03 16:42:27 -0300

    faz bus ca pelo nome  do participante

--- src/routes/get-event-attendees.ts
+++ src/routes/get-event-attendees.ts
@@ -12,25 +12,52 @@ export async function getEventAttendees(app: FastifyInstance) {
           eventId: z.string().uuid(),
         }),
         querystring: z.object({
-          pageIndex: z.string().nullable().default("0").transform(Number),
+          query: z.string().nullish(),
+          pageIndex: z.string().nullish().default("0").transform(Number),
         }),
         response: {},
       },
     },
     async (request, reply) => {
       const { eventId } = request.params;
-      const { pageIndex } = request.query;
+      const { pageIndex, query } = request.query;
 
       const attendees = await prisma.attendee.findMany({
-        where: {
-          eventId,
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          createdAt: true,
+          checkIn: {
+            select: {
+              createdAt: true,
+            },
+          },
         },
+        where: query
+          ? {
+              eventId,
+              name: {
+                contains: query,
+              },
+            }
+          : {
+              eventId,
+            },
         take: 10,
         skip: pageIndex * 10,
       });
 
       return reply.send({
-        attendees,
+        attendees: attendees.map((attendee) => {
+          return {
+            id: attendee.id,
+            name: attendee.name,
+            email: attendee.email,
+            createdAt: attendee.createdAt,
+            checkedInAt: attendee.checkIn?.createdAt,
+          };
+        }),
       });
     }
   );
